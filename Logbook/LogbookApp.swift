86512fc54eb87106b52fbbc05116dccfde6e12d0
Jandre <janbadtech@gmail.com>
2024-03-05 10:55:59 +0200

    Initial Commit

---
+++ Logbook/LogbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  LogbookApp.swift
+//  Logbook
+//
+//  Created by Jandre Badenhorst on 2024/03/05.
+//
+
+import SwiftUI
+
+@main
+struct LogbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
